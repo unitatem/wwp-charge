ec1a60f6886a37590b7d801f939467ed046628b8
Zuza Bednarska <zk.bednarska@gmail.com>
2018-02-15 21:36:07 +0100

    POC solver/Test

---
+++ src/solver/Test.java
@@ -0,0 +1,18 @@
+package solver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import wwp.AgglomerationList;
+import wwp.Location;
+import wwp.LocationsKeeper;
+
+public class Test {
+
+    void Test(LocationsKeeper lk){
+        //sprobuj tutaj wyswietlic zawartosc adresow w jednej aglomeracji please i uwzglednij to w Main
+        // moje proby nie zadzialaly
+    }
+}
+
+
