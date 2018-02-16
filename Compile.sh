a021316d23bc45e4857996189a07737ad49591b5
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-02-16 12:20:51 +0100

    Fix in build script

--- Compile.sh
+++ Compile.sh
@@ -12,11 +12,9 @@ module load java
 
 
 # pull git repo
-cd wwp-charge/
 git pull
-cd ..
 
 
 # compile Java code
-javac -d bin -sourcepath src -cp lib/PCJ-5.0.6.jar:lib/OSMonaut-1.1-SNAPSHOT.jar src/main/java/Main.java
+javac -d bin -sourcepath src -cp lib/PCJ-5.0.6.jar:lib/OSMonaut-1.1-SNAPSHOT.jar src/Main.java
 
